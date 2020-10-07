46399b211eb32d13c91d60f7842b6e6dd7b2ef02
Connor Clark <cjamcl@gmail.com>
2020-10-07 01:20:27 -0500

    extract all tile pages

--- src/main.py
+++ src/main.py
@@ -14,23 +14,37 @@ if __name__ == "__main__":
 
     print('num tiles', len(r.tiles))
     print('num combos', len(r.combos))
-    
+
     # save 400 at a time
-    num_sprites_to_render_side = 20  
-    img = Image.new('RGB', (num_sprites_to_render_side*16,num_sprites_to_render_side*16))
-    pixels = img.load()
-    for tile_index in range(num_sprites_to_render_side*num_sprites_to_render_side):
-      tile = r.tiles[tile_index]
-      spritesheet_x = (tile_index % num_sprites_to_render_side) * 16
-      spritesheet_y = int(tile_index / num_sprites_to_render_side) * 16
-      
-      for tx in range(16):
-        for ty in range(16):
-          tile_offset = tx + ty * 16
-          color_index = tile[tile_offset]
-          color = int(color_index / 25 * 256)
-          
-          x = spritesheet_x + tx
-          y = spritesheet_y + ty
-          pixels[x,y] = (color, color, color)
-    img.save('output/tmp.png')
+    tiles_per_row = 20
+    rows_per_page = 13
+    sprite_size = 16
+
+    tile_index = 0
+    page_index = 0
+    while tile_index < len(r.tiles):
+      img = Image.new('RGB', (tiles_per_row * sprite_size, rows_per_page * sprite_size))
+      pixels = img.load()
+      for tile_index_offset in range(tiles_per_row * rows_per_page):
+        index = tile_index + tile_index_offset
+        if index >= len(r.tiles):
+          break
+
+        tile = r.tiles[index]
+        spritesheet_x = (tile_index_offset % tiles_per_row) * sprite_size
+        spritesheet_y = int(tile_index_offset / tiles_per_row) * sprite_size
+
+        for tx in range(sprite_size):
+          for ty in range(sprite_size):
+            tile_offset = tx + ty * sprite_size
+            color_index = tile[tile_offset]
+            color = int(color_index / 25 * 256)
+            
+            x = spritesheet_x + tx
+            y = spritesheet_y + ty
+            pixels[x,y] = (color, color, color)
+        
+        tile_index += 1
+
+      img.save(f'output/tiles_{page_index}.png')
+      page_index += 1
