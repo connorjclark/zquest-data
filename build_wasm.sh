a37730aa77174f7250b884dafe0bbbf141188e9e
Connor Clark <cjamcl@gmail.com>
2022-02-19 17:17:48 -0800

    tweak

--- build_wasm.sh
+++ build_wasm.sh
@@ -12,4 +12,4 @@ emcc -o build/wasm/zc.js $OPTIMIZE_FLAGS \
   -s FORCE_FILESYSTEM=1 \
   -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap","FS"]' \
   -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
-  lib/fake-allegro.c lib/decode.c third_party/allegro/src/file.c third_party/allegro/src/unicode.c third_party/allegro/src/libc.c third_party/allegro/src/lzss.c third_party/allegro/src/unix/*.c
+  lib/fake-allegro.c lib/decode.c third_party/allegro/src/file.c third_party/allegro/src/unicode.c third_party/allegro/src/libc.c third_party/allegro/src/lzss.c third_party/allegro/src/unix/ufile.c
