3a26fddff2dc022b703d5a09d8c81be61cb820f6
Connor Clark <cjamcl@gmail.com>
2022-06-05 21:45:34 -0700

    examples/print_header.py

---
+++ examples/print_header.py
@@ -0,0 +1,12 @@
+import context
+import argparse
+from zquest.extract import ZeldaClassicReader
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('input', type=str)
+args = parser.parse_args()
+
+reader = ZeldaClassicReader(args.input)
+reader.read_qst()
+
+print(reader.header)
